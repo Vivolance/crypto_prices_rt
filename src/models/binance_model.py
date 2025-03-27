26c47d108d978a87a98c72ef52fc98b486399d94
Elson Chan <elsonccjgmail.com>
2025-03-27 14:13:09 +0800

    Created binance dataclass

---
+++ src/models/binance_model.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+
+class BinanceCryptoData(BaseModel):
+    e: str  # Event type
+    E: int  # Event time
+    s: str  # Symbol
+    c: str  # Close
+    o: str  # Open
+    h: str  # High
+    l: str  # Low
+    v: str  # Total traded base asset volume
+    q: str  # Total traded quote asset volume
