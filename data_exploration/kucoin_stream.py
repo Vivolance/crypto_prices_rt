adedcd47ae7dce7a80f031c49465385ee36f130a
Elson Chan <elsonccjgmail.com>
2025-03-28 16:47:18 +0800

    Added stream_to_csv method

--- data_exploration/kucoin_stream.py
+++ data_exploration/kucoin_stream.py
@@ -2,6 +2,8 @@ import asyncio
 import json
 from typing import AsyncGenerator, Any
 
+import aiocsv
+import aiofiles
 import aiohttp
 from aiohttp import WSMessage
 
@@ -121,11 +123,53 @@ async def socket_request() -> AsyncGenerator[dict[str, Any], None]:
         raise Exception(f"Unexpected error occurred: {e}") from e
 
 
-async def main() -> None:
-    async for ticker in socket_request():
-        print(ticker)
+async def stream_to_csv(file_path: str) -> None:
+    # Define the CSV header you want:
+    header: list[str] = [
+        "topic",
+        "type",
+        "subject",
+        "bestAsk",
+        "bestAskSize",
+        "bestBid",
+        "bestBidSize",
+        "price",
+        "sequence",
+        "size",
+        "time",
+    ]
+
+    # Open file for asynchronous writing.
+    async with aiofiles.open(file_path, mode="w", newline="") as af:
+        writer = aiocsv.AsyncDictWriter(af, fieldnames=header)
+        await writer.writeheader()
+
+        # Process each message from the socket stream.
+        async for message in socket_request():
+            # Filter to process only ticker messages.
+            if message.get("type") != "message" or "subject" not in message:
+                continue
+
+            # Flatten the message. To be done in spark as transformation step
+            flattened = {
+                "topic": message.get("topic", ""),
+                "type": message.get("type", ""),
+                "subject": message.get("subject", ""),
+                "bestAsk": message.get("data", {}).get("bestAsk", ""),
+                "bestAskSize": message.get("data", {}).get("bestAskSize", ""),
+                "bestBid": message.get("data", {}).get("bestBid", ""),
+                "bestBidSize": message.get("data", {}).get("bestBidSize", ""),
+                "price": message.get("data", {}).get("price", ""),
+                "sequence": message.get("data", {}).get("sequence", ""),
+                "size": message.get("data", {}).get("size", ""),
+                "time": message.get("data", {}).get("time", ""),
+            }
+            await writer.writerow(flattened)
+            # Print for observation.
+            print(flattened)
 
 
 if __name__ == "__main__":
+    file_path: str = "./data_exploration/output/kucoin_raw.csv"
     event_loop = asyncio.new_event_loop()
-    event_loop.run_until_complete(main())
+    event_loop.run_until_complete(stream_to_csv(file_path))
