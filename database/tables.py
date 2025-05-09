14e89351d8983f14eb45ad86cd92e7abb7609197
Elson Chan <elsonccjgmail.com>
2025-05-09 16:45:23 +0800

    Created sqlalchemy tables

---
+++ database/tables.py
@@ -0,0 +1,28 @@
+from sqlalchemy import MetaData, TEXT, Table, Column, FLOAT, DateTime
+
+metadata: MetaData = MetaData()
+
+
+kucoin_table: Table = Table(
+    "kucoin_result",
+    metadata,
+    Column("symbol", TEXT, primary_key=True),
+    Column("price", FLOAT, nullable=False),
+    Column("time", DateTime, nullable=False),
+    Column("source", TEXT, nullable=False),
+    Column("created_at", DateTime, nullable=False)
+)
+"""
+
+"""
+
+
+binance_table: Table = Table(
+    "kucoin_result",
+    metadata,
+    Column("symbol", TEXT, primary_key=True),
+    Column("price", FLOAT, nullable=False),
+    Column("time", DateTime, nullable=False),
+    Column("source", TEXT, nullable=False),
+    Column("created_at", DateTime, nullable=False)
+)
