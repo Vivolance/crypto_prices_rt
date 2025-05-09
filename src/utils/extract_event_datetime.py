7a743e39d02fd31a22ff0083f54521d8806926ec
Elson Chan <elsonccjgmail.com>
2025-05-09 18:28:25 +0800

    Remove class usage

--- src/utils/extract_event_datetime.py
+++ src/utils/extract_event_datetime.py
@@ -2,23 +2,22 @@ from datetime import datetime, timezone
 from typing import Literal
 
 
-class DatetimeExtractor:
-    """
-    Converts UNIX timestamp retrieved from Binance and Kucoin into datetime format
-    """
+"""
+Converts UNIX timestamp retrieved from Binance and Kucoin into datetime format
+"""
 
-    @staticmethod
-    def extract_event_datetime(source: Literal["kucoin", "binance"], message: dict) -> datetime:
-        """
-        Extracts the event time of each message from binance and kucoin
-        - binance: timestamp is at message["E"]
-        - kucoin: timestamp is at message["data"]["time"]
-        """
-        if source == "kucoin":
-            timestamp_ms = message["data"]["time"] # in ms
-        elif source == "binance":
-            timestamp_ms = message["E"] # in ms
-        else:
-            raise ValueError(f"Unknown source: {source}")
 
-        return datetime.utcfromtimestamp(timestamp_ms / 1000)
+def extract_event_datetime(source: Literal["kucoin", "binance"], message: dict) -> datetime:
+    """
+    Extracts the event time of each message from binance and kucoin
+    - binance: timestamp is at message["E"]
+    - kucoin: timestamp is at message["data"]["time"]
+    """
+    if source == "kucoin":
+        timestamp_ms = message["data"]["time"] # in ms
+    elif source == "binance":
+        timestamp_ms = message["E"] # in ms
+    else:
+        raise ValueError(f"Unknown source: {source}")
+
+    return datetime.utcfromtimestamp(timestamp_ms / 1000)
