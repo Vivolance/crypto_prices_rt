b3ef94e832a3f0ddda0bef0fd63e86e2205652e6
Elson Chan <elsonccjgmail.com>
2025-05-20 18:42:00 +0800

    black ruff

--- src/utils/extract_event_datetime.py
+++ src/utils/extract_event_datetime.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime
 from typing import Literal
 
 
@@ -7,16 +7,18 @@ Converts UNIX timestamp retrieved from Binance and Kucoin into datetime format
 """
 
 
-def extract_event_datetime(source: Literal["kucoin", "binance"], message: dict) -> datetime:
+def extract_event_datetime(
+    source: Literal["kucoin", "binance"], message: dict
+) -> datetime:
     """
     Extracts the event time of each message from binance and kucoin
     - binance: timestamp is at message["E"]
     - kucoin: timestamp is at message["data"]["time"]
     """
     if source == "kucoin":
-        timestamp_ms = message["data"]["time"] # in ms
+        timestamp_ms = message["data"]["time"]  # in ms
     elif source == "binance":
-        timestamp_ms = message["E"] # in ms
+        timestamp_ms = message["E"]  # in ms
     else:
         raise ValueError(f"Unknown source: {source}")
 
